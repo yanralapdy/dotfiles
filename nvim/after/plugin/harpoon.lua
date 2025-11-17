c7384466f8182fceaa2c077aae4389ac1ee492d0
yanral apdy <thanurking@yanrals-MacBook-Air.local>
2025-11-17 09:34:03 +0700

    feat(nvim): update plugin configurations and lazy setup
    - Modified plugin configs: harpoon, lsp, treesitter, and other custom plugins
    - Updated lazy-lock.json to reflect new plugin versions
    - Change theme in lua/aptha/lazy.lua
    - Adding tokyonight theme in lua/aptha/plugins/init.lua
    - Improved key mappings for diagnostics and help in lua/aptha/remap.lua

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -6,10 +6,11 @@ harpoon:setup()
 
 vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
 vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
---vim.keymap.set("n", "<C-H>", function() harpoon:list():select(1) end)
---vim.keymap.set("n", "<C-T>", function() harpoon:list():select(2) end)
---vim.keymap.set("n", "<C-N>", function() harpoon:list():select(3) end)
---vim.keymap.set("n", "<C-S>", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<leader>5", function() harpoon:list():select(5) end)
 
 -- Toggle previous & next buffers stored within Harpoon list
 vim.keymap.set("n", "<C-P>", function() harpoon:list():prev() end)
