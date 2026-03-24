e099074ad7210d1020df713d20f523f86ff1eb3a
yanral apdy <tnkapdy@yanrals-MacBook-Pro.local>
2026-03-24 19:33:22 +0700

    Add node PATH in nvim set.lua for LSP

--- nvim/.config/nvim/lua/aptha/set.lua
+++ nvim/.config/nvim/lua/aptha/set.lua
@@ -2,6 +2,12 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+-- Add node to PATH for LSP servers
+local nvm_node_path = vim.fn.expand("$HOME/.nvm/versions/node/v20.20.1/bin")
+if vim.fn.isdirectory(nvm_node_path) == 1 then
+    vim.env.PATH = nvm_node_path .. ":" .. vim.env.PATH
+end
+
 -- Create an autocmd to enforce number and relativenumber in Netrw (:Vex)
 vim.api.nvim_create_autocmd("FileType", {
     pattern = "netrw",
